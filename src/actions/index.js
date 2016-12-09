547735c0ae8e0615ebeba0e488ba2886c7141163
bud aminof <budaminof@gmail.com>
2016-12-09 12:34:53 -0700

    set up axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+//for making AJAX request
+import axios from 'axios';
 const API_KEY = '580a56d51527aaab9d383c6894b1cafa';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
