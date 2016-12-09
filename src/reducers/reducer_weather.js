8b24a8dd461dc60e21c0b6555652147809621761
bud aminof <budaminof@gmail.com>
2016-12-09 14:35:39 -0700

    saving data to new state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,12 @@
-export default function (state= null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state= [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+    // DON'T mutate state, we want to returning a new state. new reference not mutating.
+    // DON'T state.push( action.payload.data );
+    // return state.concat([ action.payload.data ]); // same as:
+      return [ action.payload.data, ...state ]
+  }
   return state;
 }
